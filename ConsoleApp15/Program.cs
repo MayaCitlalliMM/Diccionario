408b58e066ac281591e37f138f541ceba1ceeed1
Lab2-15 <Lab2-15@LAB2-15>
2025-03-13 18:31:51 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> colores = new Dictionary<int, string>();
+
+            colores.Add(113, "blanco");
+            colores.Add(102, "negro");
+            colores.Add(120, "cafe");
+            colores.Add(214, "beige");
+            colores.Add(178, "purpura");
+            colores.Add(40, "lila");
+            colores.Add(90, "rojo");
+            colores.Add(159, "naranja");
+            colores.Add(101, "amarillo");
+            colores.Add(46, "verde");
+        }
+    }
+}
